d526ae7cbbe671bb7c7ae243fa4b3f95f9e8da0d
Don Carroll <doncarroll38@yahoo.com>
2019-11-26 15:55:26 -0600

    Add files via upload

---
+++ OrderDataService/Models/DataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderDataService.Models
+{
+    public class DataModel
+    {
+        public long OrderID { get; set; }
+        public int ShipperID { get; set; }
+        public int DriverID { get; set; }
+        public DateTime CompletionDte{ get; set; }
+        public int Status { get; set; }
+        public string Code { get; set; }
+        public int MSA { get; set; }
+        public double Duration { get; set; }
+        public int OfferType { get; set; }
+    }
+}
--- OrderDataService/Models/DataParams.cs
+++ OrderDataService/Models/DataParams.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
+namespace OrderDataService.Models
+{
+    public class DataParams
+    {
+        public long OrderID { get; set; }
+        public int MSA { get; set; }
+        public int Status { get; set; }
+        public DateTime CompletionDte { get; set; }
+    }
+}
